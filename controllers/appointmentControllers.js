76481c2e7383040b33ce4b8ceec549b5da2d80c4
eledu <develmdq@gmail.com>
2022-02-14 12:32:19 -0300

    fix error console

--- controllers/appointmentControllers.js
+++ controllers/appointmentControllers.js
@@ -4,8 +4,7 @@ exports.createAppointment = async (req, res) => {
   try {
     const appointment = new Appointment(req.body);
     await appointment.save();
-    res.json(appointment);
-    res.send("Cita insertada correctamente");
+    res.json(appointment);   
   } catch (error) {
     console.log(error);
     res.status(400).send("Se produjo un error");
@@ -15,7 +14,6 @@ exports.getAppointments = async (req, res) => {
   try {
     const appointments = await Appointment.find();
     res.json(appointments);
-    res.send("Consulta realizada ok");
   } catch (error) {
     console.log(error);
     res.status(500).send("Se produjo un error");
